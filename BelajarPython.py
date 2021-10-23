809d5e7b914bb4cc26a52cfa02fff88ff6e2981c
fidariya <fidamufida2004@gmail.com>
2021-10-24 02:04:39 +0700

    Menambahkan file Belajar pyhton

---
+++ BelajarPython.py
@@ -0,0 +1,16 @@
+# Mengambil input
+nm = input("Siapa nama kamu: ")
+np = input ("Apa nama panggilan kamu: ")
+umur = input("Berapa umur kamu: ")
+npm = input ("Berapa nomor NPM kamu: ")
+ttl = input ("Dimana tempat kamu lahir: ")
+tanggal = input ("Tanggal berapa kamu dilahirkan: ")
+alamat = input ("Dimana kamu tinggal: ")
+telepon = input ("Berapa nomer telepon kamu: ")
+
+
+# Menampilkan output
+print ("Assalamualaikum")
+print ("Let me introduce my self. My name is" ,nm, "but you can call me",np,"My NPM is",npm,
+"I was born in",ttl,"and I am" ,umur, "years old.""I am very glad if you want to invite my house in",alamat,". So, don't forget to call me before with the number",telepon,)
+print ("Thank you.")
